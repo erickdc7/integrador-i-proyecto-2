054a6b9c7d9a9a35f03556b6161fef6e86618f58
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-11-12 12:37:01 -0500

    feat: Se modificaron los atributos de la clase Paciente

--- src/modelo/Paciente.java
+++ src/modelo/Paciente.java
@@ -1,7 +1,8 @@
 package modelo;
 
 public class Paciente {
-private int id;
+
+    private int id;
     private String nombres;
     private String apellidos;
     private String dni;
@@ -11,6 +12,91 @@ private int id;
     private String celular;
     private String direccion;
 
-     
+    public Paciente() {
+    }
+
+    public Paciente(int id, String nombres, String apellidos, String dni, int edad, String genero, String correo, String celular, String direccion) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.edad = edad;
+        this.genero = genero;
+        this.correo = correo;
+        this.celular = celular;
+        this.direccion = direccion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
 
 }
